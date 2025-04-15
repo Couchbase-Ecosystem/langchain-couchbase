ca9c96ffd8c230601a23a7e21434e12242cdc4a9
Nithish Raghunandanan <12782505+nithishr@users.noreply.github.com>
2025-04-15 22:04:49 +0200

    Add initial implementation of CouchbaseQueryVectorSearch

--- langchain_couchbase/vectorstores/__init__.py
+++ langchain_couchbase/vectorstores/__init__.py
@@ -1,3 +1,8 @@
+from langchain_couchbase.vectorstores.query_vector_store import (
+    CouchbaseQueryVectorStore,
+    DistanceStrategy,
+    IndexType,
+)
 from langchain_couchbase.vectorstores.search_vector_store import (
     CouchbaseSearchVectorStore,
 )
@@ -6,4 +11,7 @@ from langchain_couchbase.vectorstores.vectorstores import CouchbaseVectorStore
 __all__ = [
     "CouchbaseSearchVectorStore",
     "CouchbaseVectorStore",
+    "CouchbaseQueryVectorStore",
+    "DistanceStrategy",
+    "IndexType",
 ]
